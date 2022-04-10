04eac915ee1335b9a702caa473bf6e0a0217101a
maxpermal <max.permalnaick@gmail.com>
2022-04-10 23:31:15 +0200

    update playercontroller

--- Assets/Scripts/ActorController.cs
+++ Assets/Scripts/ActorController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class ActorController : MonoBehaviour
 {
-    protected GameManager gameManager;
-    public ActorType type;
     public GameObject bullet;
 
+    protected GameManager gameManager;
+    [SerializeField] protected ActorType type;
+    public ActorType Type => type;
+
     protected ActorProfile profile;
+    protected ActorInput inputs;
 
     protected Rigidbody body;
-    protected Vector3 direction;
+    [SerializeField] protected Vector3 direction;
+    [SerializeField] protected Vector3 storeDirection;
+    [SerializeField] protected bool shootInput;
+
+    public bool ShootInput => shootInput;
+    public Vector3 Direction => direction;
 
     [SerializeField] protected float durationCooldown = 5f;
     [SerializeField] protected float cooldownShoot;
@@ -24,49 +32,53 @@ public class ActorController : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         body = gameObject.GetComponent<Rigidbody>();
         profile = gameObject.GetComponent<ActorProfile>();
+        inputs = new ActorInput();
     }
 
     virtual protected void Start()
     {
         direction = Vector3.forward;
+        storeDirection = Vector3.zero;
     }
 
     // Update is called once per frame
     virtual protected void Update()
     {
-
+        if(inputs.attack)
+        {
+            Shoot(storeDirection);
+        }
     }
 
     virtual protected void FixedUpdate()
     {
-        if (profile.isDead) return;
-        MovePlayer(direction);
+        MovePlayer(inputs.vertical, inputs.horizontal);
     }
 
-    virtual protected void MovePlayer(Vector3 moveDirection)
+    virtual protected void MovePlayer(float vertical, float horizontal)
     {
-        var vertical = moveDirection.z;
-        var horizontal = moveDirection.x;
+        vertical = Utils.Round(vertical);
+        horizontal = Utils.Round(horizontal);
         body.MovePosition(body.position + vertical * Vector3.forward * profile.speed * Time.deltaTime + horizontal * Vector3.right * profile.speed * Time.deltaTime);
         LastDirection(horizontal, vertical);
     }
 
     private void LastDirection(float horizontal, float vertical)
     {
-        var oldirection = direction;
-        direction.x = Utils.Round(horizontal);
-        direction.z = Utils.Round(vertical);
-        if (direction.magnitude == 0f)
+        direction.x = horizontal;
+        direction.z = vertical;
+        if (direction.magnitude > 0)
         {
-            direction = oldirection;
+            storeDirection = direction;
         }
+        Debug.Log(storeDirection);
     }
 
-    virtual protected void Shoot(Vector3 shootdirection)
+    virtual protected void Shoot(Vector3 shootdir)
     {
         if (canShoot)
         {
-            gameManager.Shoot(gameObject, bullet, shootdirection);
+            gameManager.Shoot(gameObject, bullet, shootdir);
             StartCoroutine(ShootCorountine());
         }
 
@@ -82,4 +94,12 @@ public class ActorController : MonoBehaviour
             canShoot = true;
         }
     }
+}
+
+
+public struct ActorInput
+{
+    public float vertical;
+    public float horizontal;
+    public bool attack;
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,83 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : ActorController
 {
-    private GameManager gameManager;
-    public ActorType type;
-    public GameObject bullet;
-
-    ActorProfile profile;
-
-    private Rigidbody body;
-    private Vector3 direction;
-
-    void Awake()
-    {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        body = GetComponent<Rigidbody>();
-        profile = gameObject.GetComponent<ActorProfile>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        direction = Vector3.forward;
-    }
-
     public void StartGame()
     {
         body.velocity = Vector3.zero;
         profile.StartGame();
+        canShoot = true;
     }
 
-    private Vector3 PlayerPos;
-    // Update is called once per frame
-    void Update()
+    override protected void Update()
     {
         if (profile.isDead) return;
-        Shoot();
+        inputs.vertical = Input.GetAxis("Vertical");
+        inputs.horizontal = Input.GetAxis("Horizontal");
+        inputs.attack = Input.GetKeyDown(KeyCode.LeftControl);
+        base.Update();
     }
 
-    void FixedUpdate()
+    override protected void FixedUpdate()
     {
         if (profile.isDead) return;
-        PlayerPos = transform.position;
-        MovePlayer();
-    }
-
-    private void MovePlayer()
-    {
-        // var dt = Time.deltaTime;
-        // var v0 = 0;
-        // var F = 0;
-        // var m = 1;
-        // var x0 = 0;
-        // var v = v0 + F/m * dt;
-        // var x = x0 + v * dt;
-
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        body.MovePosition(body.position + vertical * Vector3.forward * profile.speed * Time.deltaTime + horizontal * Vector3.right * profile.speed * Time.deltaTime);
-        LastDirection(horizontal, vertical);
-    }
-
-    private void LastDirection(float horizontal, float vertical)
-    {
-        var oldirection = direction;
-        direction.x = Utils.Round(horizontal);
-        direction.z = Utils.Round(vertical);
-        if (direction.magnitude == 0f)
-        {
-            direction = oldirection;
-        }
-    }
-
-    private void Shoot()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            gameManager.Shoot(gameObject, bullet, new Vector3(direction.x, 0, direction.z));
-        }
+        base.FixedUpdate();
     }
 }
